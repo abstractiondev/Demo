24bb971d0b6bedf865d3e04b4e110e6aa6d70d2c
Kalle Launiala <kalle.launiala@protonit.net>
2011-09-24 00:29:38 +0300

    Restructured demo to display files better

--- LogicalOperationDemo/Abstractions/AbstractionContent/Operation/Out/OperationContent_v1_0.designer.cs
+++ LogicalOperationDemo/Abstractions/AbstractionContent/Operation/Out/OperationContent_v1_0.designer.cs
@@ -17,15 +17,9 @@ using System;
 				}
 				public static SortNumbersEvensBeforeOddsReturnValue Execute(SortNumbersEvensBeforeOddsParameters parameters)
 		{
-				        DateTime perfMonitorTotalTimeMs_MaximumTimeAllowed_Start = DateTime.Now;
-					PrepareParameters(parameters);
+						PrepareParameters(parameters);
 					int[] SortNumbersOutput = SortNumbersEvensBeforeOddsImplementation.ExecuteMethod_SortNumbers(parameters.DataArray, parameters.AscendingOrder);		
 				int[] SortEvensBeforeOddsOutput = SortNumbersEvensBeforeOddsImplementation.ExecuteMethod_SortEvensBeforeOdds(SortNumbersOutput);		
-					TimeSpan TotalTimeMs_MaximumTimeAllowed_duration = DateTime.Now - perfMonitorTotalTimeMs_MaximumTimeAllowed_Start;
-            if(TotalTimeMs_MaximumTimeAllowed_duration.TotalMilliseconds > 1000)
-            {
-                throw new TimeoutException(String.Format("Performance not withing requirement: MaximumTimeAllowed requires 1000 ms, it took {0} ms", TotalTimeMs_MaximumTimeAllowed_duration.TotalMilliseconds));
-            }
 				SortNumbersEvensBeforeOddsReturnValue returnValue = SortNumbersEvensBeforeOddsImplementation.Get_ReturnValue(SortEvensBeforeOddsOutput);
 		return returnValue;
 				}
