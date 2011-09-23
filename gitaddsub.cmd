f0cef1b40ebbde3b5ea805341be87254e91150d4
Kalle Launiala <kalle.launiala@protonit.net>
2011-09-24 00:09:21 +0300

    Removed SSH linked submodules to be git readonly linked

--- gitaddsub.cmd
+++ gitaddsub.cmd
@@ -1,4 +1,4 @@
 @echo Abstraction: %1 Adding to Demo: %2
 
-git submodule add git@github.com:abstractiondev/%1 %2/Abstractions/%1
+git submodule add git://github.com/abstractiondev/%1 %2/Abstractions/%1
 @echo Enter afterwards: git submodule update --init
