3168e58a440775e3d1733c8ac2ac97662aaa2daa
Kalle Launiala <kalle.launiala@protonit.net>
2011-09-25 18:52:56 +0300

    Updated print messages

--- LogicalOperationDemo/Apps/EnforcedRequirementsDemo/EnforcedRequirements/Program.cs
+++ LogicalOperationDemo/Apps/EnforcedRequirementsDemo/EnforcedRequirements/Program.cs
@@ -15,12 +15,23 @@ namespace EnforcedRequirements
                     DataArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
                 });
             int[] okData = okResult.SortedArray;
+            print10First(okData);
 
             var supposedToFailPerfReq = SortNumbersEvensBeforeOdds.Execute(
                 new SortNumbersEvensBeforeOddsParameters() {AscendingOrder = false,
                     DataArray = new int[5000000]
                 });
             int[] notSupposedToGetHere = supposedToFailPerfReq.SortedArray;
+            print10First(notSupposedToGetHere);
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+
+        private static void print10First(int[] intArray)
+        {
+            string printStr =
+                String.Join(", ", intArray.Take(10).Select(i => i.ToString()).ToArray());
+            Console.WriteLine(printStr);
         }
     }
 }
